0c99c13434b8949f9f052d4ee6a4ae8b25adc041
MNavarro1997 <mnavarro@gteam.es>
2022-07-16 10:33:28 +0200

    created again vue.js and moved images

---
+++ Nutrilia-Front/nutrilia/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div id = "app">
+  <NavComp />
+  <div class = "message">
+    <div class = "container">
+      <div class = "row">
+        <div class = "col-md-12">
+          <h1>Bienvenido a Nutrilia</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Quisquam, quidem, quisquam.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import NavComp from './components/NavComp.vue'
+export default {
+  name: 'App',
+  components: {
+    NavComp
+  }
+}
+</script>
+
+<style>
+
+</style>
